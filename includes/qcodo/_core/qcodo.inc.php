ea8ac1f49d1cfb01ed186746a003bc3cf11c2b9c
Admin <admin@ils.com>
2021-02-10 04:27:59 +0200

    deployment

--- includes/qcodo/_core/qcodo.inc.php
+++ includes/qcodo/_core/qcodo.inc.php
@@ -29,7 +29,7 @@
 	 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 	 * SOFTWARE.
 	 */
-
+console.log('Entering qcodo.inc');
 	// Versioning Information
 	define('QCODO_VERSION', '0.4.22');
 
