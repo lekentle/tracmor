df34e98f3a7676dad53eb91817c6e9e3b8f462df
Admin <admin@ils.com>
2021-02-25 16:14:25 +0200

    deployment

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "Only services are allowed to be invoked.";
