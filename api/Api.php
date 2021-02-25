ce0186f4ee5cd714932e7bffd024dacac07a77f2
Admin <admin@ils.com>
2021-02-25 20:42:55 +0200

    deployment

--- api/Api.php
+++ api/Api.php
@@ -1,93 +1,95 @@
 <?php 
                         
         error_log("Entering Api"); 
+       
+	require_once('../includes/prepend.inc.php');
+	QApplication::Authenticate(3);
+	require_once(__FORMBASE_CLASSES__ . '/UserAccountListFormBase.class.php');
+
+
         
-	require_once '../includes/configuration.inc.php';
-	require_once '../includes/data_classes/UserAccount.class.php';
+        class Api{
+            
+            
+            private $apicall;
+            private $response;
+            private $request;
+            
+            function __construct($call, $payload) {
+                
+                $this->response =  array();
+                $this->apicall = $params['apicall'];
+                $this->request = $payload;
+                //Default response
+                $response['error'] = true; 
+                $response['message'] = 'Invalid Operation Called';
+             }
+
+            
+            function isTheseParametersAvailable($params){
+
+                foreach($params as $param){
+                        if(!isset($payload[$param])){
+                                return false; 
+                        }
+                }
+                return true; 
+            }
+            
+            function syncLoookups (){
+                
+                
+            }
+            
+            function login (){
+                
+               
+       		if(isTheseParametersAvailable(array('username', 'password'))){
+					
+                    $username = $payload['username'];
+                    $password = $payload['password']; 
+
+                    $objUserAccount = UserAccount::LoadByUsername($username);
+
+
+                    // Check if that username exists
+                    if (!$objUserAccount) {
+                        $response['error'] = true; 
+                        $response['message'] = 'Invalid username or password.'; 
+
+                        // Check that the user account is Active
+                    }elseif (!$objUserAccount->ActiveFlag) {
+                        $response['error'] = true; 
+                        $response['message'] = 'The account is disabled. please contact the administrator.';
+
+                    }
+                    // Check to see if the password hashes match
+                    elseif (!QApplication::CheckPassword(sha1($password), $objUserAccount->PasswordHash)) {
+                        $response['error'] = true; 
+                        $response['message'] = 'Wrong password.';
+                    }
+                    else {
+
+                        $response['error'] = false; 
+                        $response['message'] = 'Login successfull'; 
+                        $response['user'] = $user; 
+
+                    }
+                }
+            }
+
+            function uploadInventory (){
+                
+            }
+            
+            public function getResponse(){
+                
+            return $this->response;
+            }
+            
+        }
+
+        $api = new Api(_GET['apicall'], _POST);
         
-	$response = array();
+        echo json_encode($api->getResponse());
 	
-	if(isset($_GET['apicall'])){
-		
-		switch($_GET['apicall']){
-			
-			case 'sync':
-				if(isTheseParametersAvailable(array('username','email','password','gender'))){
-
-					
-				}else{
-					$response['error'] = true; 
-					$response['message'] = 'required parameters are not available'; 
-				}
-				
-			break; 
-                        
-			case 'upload':
-				if(isTheseParametersAvailable(array('username','email','password','gender'))){
-
-					
-				}else{
-					$response['error'] = true; 
-					$response['message'] = 'required parameters are not available'; 
-				}
-				
-			break; 
-			
-			case 'login':
-				
-				if(isTheseParametersAvailable(array('username', 'password'))){
-					
-                                    $username = $_POST['username'];
-                                    $password = $_POST['password']; 
-
-                                    $objUserAccount = UserAccount::LoadByUsername($username);
-
-
-                                    // Check if that username exists
-                                    if (!$objUserAccount) {
-                                        $response['error'] = true; 
-                                        $response['message'] = 'Invalid username or password.'; 
-
-                                        // Check that the user account is Active
-                                    }elseif (!$objUserAccount->ActiveFlag) {
-                                        $response['error'] = true; 
-                                        $response['message'] = 'The account is disabled. please contact the administrator.';
-
-                                    }
-                                    // Check to see if the password hashes match
-                                    elseif (!QApplication::CheckPassword(sha1($password), $objUserAccount->PasswordHash)) {
-                                        $response['error'] = true; 
-                                        $response['message'] = 'Wrong password.';
-                                    }
-                                    else {
-                                        
-                                        $response['error'] = false; 
-                                        $response['message'] = 'Login successfull'; 
-                                        $response['user'] = $user; 
-
-                                    }
-						
-				}
-			break; 
-			
-			default: 
-				$response['error'] = true; 
-				$response['message'] = 'Invalid Operation Called';
-		}
-		
-	}else{
-		$response['error'] = true; 
-		$response['message'] = 'Invalid API Call';
-	}
-	
-	echo json_encode($response);
-	
-	function isTheseParametersAvailable($params){
-		
-		foreach($params as $param){
-			if(!isset($_POST[$param])){
-				return false; 
-			}
-		}
-		return true; 
-	}
