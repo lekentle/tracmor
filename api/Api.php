7cf5720ff7c496a8e05f85c3dec2c34aa7a385ed
Admin <admin@ils.com>
2021-02-25 16:31:44 +0200

    deployment

--- api/Api.php
+++ api/Api.php
@@ -1,8 +1,93 @@
-<?php
+<?php 
+                        
+        error_log("Entering Api"); 
+        
+	require_once '../includes/configuration.inc.php';
+	require_once '../includes/UserAccount.class.php';
+        
+	$response = array();
+	
+	if(isset($_GET['apicall'])){
+		
+		switch($_GET['apicall']){
+			
+			case 'sync':
+				if(isTheseParametersAvailable(array('username','email','password','gender'))){
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+					
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'required parameters are not available'; 
+				}
+				
+			break; 
+                        
+			case 'upload':
+				if(isTheseParametersAvailable(array('username','email','password','gender'))){
 
+					
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'required parameters are not available'; 
+				}
+				
+			break; 
+			
+			case 'login':
+				
+				if(isTheseParametersAvailable(array('username', 'password'))){
+					
+                                    $username = $_POST['username'];
+                                    $password = $_POST['password']; 
+
+                                    $objUserAccount = UserAccount::LoadByUsername($username);
+
+
+                                    // Check if that username exists
+                                    if (!$objUserAccount) {
+                                        $response['error'] = true; 
+                                        $response['message'] = 'Invalid username or password.'; 
+
+                                        // Check that the user account is Active
+                                    }elseif (!$objUserAccount->ActiveFlag) {
+                                        $response['error'] = true; 
+                                        $response['message'] = 'The account is disabled. please contact the administrator.';
+
+                                    }
+                                    // Check to see if the password hashes match
+                                    elseif (!QApplication::CheckPassword(sha1($password), $objUserAccount->PasswordHash)) {
+                                        $response['error'] = true; 
+                                        $response['message'] = 'Wrong password.';
+                                    }
+                                    else {
+                                        
+                                        $response['error'] = false; 
+                                        $response['message'] = 'Login successfull'; 
+                                        $response['user'] = $user; 
+
+                                    }
+						
+				}
+			break; 
+			
+			default: 
+				$response['error'] = true; 
+				$response['message'] = 'Invalid Operation Called';
+		}
+		
+	}else{
+		$response['error'] = true; 
+		$response['message'] = 'Invalid API Call';
+	}
+	
+	echo json_encode($response);
+	
+	function isTheseParametersAvailable($params){
+		
+		foreach($params as $param){
+			if(!isset($_POST[$param])){
+				return false; 
+			}
+		}
+		return true; 
+	}
